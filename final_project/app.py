ee3fa77dbc0396f78f40a6ab450144e9c323d47e
trndav <farsiteguide@gmail.com>
2024-01-26 13:12:31 +0100

    Added login, add/delete posts bootstrap styling

--- final_project/app.py
+++ final_project/app.py
@@ -1,8 +1,10 @@
 
-from flask import Flask, render_template, session, request, redirect
+from flask import Flask, render_template, session, request, redirect, url_for
 from flask_session import Session
 from cs50 import SQL
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
+from datetime import datetime
 
 # Configure application
 app = Flask(__name__)
@@ -22,9 +24,19 @@ db = SQL("sqlite:///users.db")
 def index():
 
     logged_in = "user_id" in session
+    if logged_in:
+
+        # Get username of logged user
+        result = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+        username = result[0]['username']
+    else:
+        username = None
+
+    # Get all posts from the database
+    posts = db.execute("SELECT * FROM posts")
 
     # If index.html request is GET
-    return render_template("index.html", message="Welcome!", logged_in=logged_in)
+    return render_template("index.html", message="", logged_in=logged_in, username=username, posts=posts)
 
 # Register.html
 @app.route("/register", methods=["GET", "POST"])
@@ -49,7 +61,7 @@ def register():
         else:
             hashed_password = generate_password_hash(password, method='pbkdf2', salt_length=16)
             db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hashed_password)
-            return render_template("login.html", message="Successful registration! You can login now.")
+            return render_template("login.html", message="Successful registration! You can login now.")        
 
     # If register reguest is GET
     return render_template("register.html")
@@ -82,8 +94,13 @@ def login():
         # Handle logged_in defined in index.html for different menu options
         logged_in = "user_id" in session
 
+        # Get username of logged user
+        result = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+        username = result[0]['username']
+
         # Redirect logged user to homepage
-        return render_template("index.html", message="You are logged in.", logged_in=logged_in)
+        # return render_template("index.html", message="You are logged in.", logged_in=logged_in, username=username)
+        return redirect(url_for("index"))
 
     # If login request is GET 
     return render_template("login.html")
@@ -96,10 +113,44 @@ def logout():
     session.clear()
 
     # Redirect user to login form
-    return render_template("index.html", message="You are logged out.")
+    return render_template("login.html", message="You have logged out.")
 
 # Create post.html
 @app.route("/post", methods=["GET", "POST"])
+@login_required
 def post():
 
-    return render_template("post.html")
+    logged_in = "user_id" in session
+
+    # Get username of logged user
+    result = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+    username = result[0]['username']
+
+    if request.method == "POST":
+        current_user = session.get('user_id')
+        title = request.form.get("title")
+        post = request.form.get("post")
+        current_datetime = datetime.now()
+
+        db.execute("INSERT INTO posts (user_id, title, post, time) VALUES (?, ?, ?, ?)",
+                    current_user, title, post, current_datetime)
+        
+        return render_template("post.html", message="Post published successfuly.", username=username, logged_in=logged_in)
+
+    return render_template("post.html", username=username, logged_in=logged_in)
+
+# Delete post
+@app.route("/delete/<int:post_id>", methods=["POST"])
+def delete(post_id):
+
+    #logged_in = "user_id" in session
+
+    # Get username of logged user
+    #result = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+    #username = result[0]['username']
+
+    if request.method == "POST":
+        # Delete the post with the given post_id from the database
+        db.execute("DELETE FROM posts WHERE id = ?", post_id)
+        return redirect(url_for("index"))
+        # return render_template("index.html", message="Post deleted.", username=username, logged_in=logged_in)
